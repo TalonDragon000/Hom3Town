eb1fc9a9011107c2da2eca43541c114334e0efb5
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2024-11-29 21:05:45 -1000

    Update vite.config.js
    added a build config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,9 @@ import vue from '@vitejs/plugin-vue'; // If using Vue
 
 export default defineConfig({
     plugins: [vue()], // Include this if using Vue
+    build: {
+        outDir: 'dist', // Ensure the output directory is set correctly
+    },
     server: {
         port: 3001, // Change this if you want a different port for the Vite dev server
         // https: true // Enable HTTPS for the Vite dev server
