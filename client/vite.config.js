3ebd3660750edce04d97b5b5d0242fcb7fbc7c1a
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2024-11-29 19:06:40 -1000

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,1 +1,10 @@
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue'; // If using Vue
 
+export default defineConfig({
+    plugins: [vue()], // Include this if using Vue
+    server: {
+        port: 3001, // Change this if you want a different port for the Vite dev server
+        // https: true // Enable HTTPS for the Vite dev server
+    },
+});
